0002a21a3d06776223fb842bd729c3702b98ca48
Felipe Sales <soufeliposales@gmail.com>
2022-01-01 14:47:17 -0300

    configuring Modular structure and yours fundamentals

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Modular App',
+    ).modular();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'app/app_module.dart';
+import 'app/app_widget.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Modular Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
+  runApp(
+    ModularApp(
+      module: AppModule(),
+      child: const AppWidget(),
+    ),
+  );
 }
